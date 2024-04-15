71690ec026da03fd418f9c431962fabe911c3f2e
Samuel <sam.rodr.g@gmail.com>
2024-04-15 20:47:17 +0000

    Add cargo features.

--- doc/trait.impl/zng_layout/unit/trait.Layout2d.js
+++ doc/trait.impl/zng_layout/unit/trait.Layout2d.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "zng":[],
 "zng_app":[["impl Layout2d for <a class=\"struct\" href=\"zng_app/widget/border/struct.CornerRadius.html\" title=\"struct zng_app::widget::border::CornerRadius\">CornerRadius</a>"]],
-"zng_color":[["impl Layout2d for <a class=\"struct\" href=\"zng_color/filter/struct.Filter.html\" title=\"struct zng_color::filter::Filter\">Filter</a>"],["impl Layout2d for <a class=\"enum\" href=\"zng_color/gradient/enum.LinearGradientAxis.html\" title=\"enum zng_color::gradient::LinearGradientAxis\">LinearGradientAxis</a>"]],
+"zng_color":[["impl Layout2d for <a class=\"enum\" href=\"zng_color/gradient/enum.LinearGradientAxis.html\" title=\"enum zng_color::gradient::LinearGradientAxis\">LinearGradientAxis</a>"],["impl Layout2d for <a class=\"struct\" href=\"zng_color/filter/struct.Filter.html\" title=\"struct zng_color::filter::Filter\">Filter</a>"]],
 "zng_layout":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
