4ab0317dff6dc6b5118baeb0d4156d051f277f7a
Samuel <sam.rodr.g@gmail.com>
2025-02-02 01:44:17 +0000

    v0.13.11

--- doc/trait.impl/core/future/future/trait.Future.js
+++ doc/trait.impl/core/future/future/trait.Future.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zng_task",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"zng_task/struct.SignalOnce.html\" title=\"struct zng_task::SignalOnce\">SignalOnce</a>"]]]]);
+    var implementors = Object.fromEntries([["zng_task",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/future/future/trait.Future.html\" title=\"trait core::future::future::Future\">Future</a> for <a class=\"struct\" href=\"zng_task/struct.SignalOnce.html\" title=\"struct zng_task::SignalOnce\">SignalOnce</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
