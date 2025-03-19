00c357440d1def7e7b3ce7321cdbcb14a1089b2b
Samuel <sam.rodr.g@gmail.com>
2025-03-19 16:39:56 +0000

    v0.14.1

--- doc/trait.impl/core/ops/arith/trait.Rem.js
+++ doc/trait.impl/core/ops/arith/trait.Rem.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zng_unit",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a> for <a class=\"struct\" href=\"zng_unit/struct.Dip.html\" title=\"struct zng_unit::Dip\">Dip</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a> for <a class=\"struct\" href=\"zng_unit/struct.Px.html\" title=\"struct zng_unit::Px\">Px</a>"]]]]);
+    var implementors = Object.fromEntries([["zng_unit",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a> for <a class=\"struct\" href=\"zng_unit/struct.Dip.html\" title=\"struct zng_unit::Dip\">Dip</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/arith/trait.Rem.html\" title=\"trait core::ops::arith::Rem\">Rem</a> for <a class=\"struct\" href=\"zng_unit/struct.Px.html\" title=\"struct zng_unit::Px\">Px</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
