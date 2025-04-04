f2177f8f218809f553281439b324ce499fe2afb1
Samuel <sam.rodr.g@gmail.com>
2025-04-04 16:52:16 +0000

    v0.14.2

--- doc/trait.impl/core/fmt/trait.Write.js
+++ doc/trait.impl/core/fmt/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zng_txt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"zng_txt/struct.Txt.html\" title=\"struct zng_txt::Txt\">Txt</a>"]]]]);
+    var implementors = Object.fromEntries([["zng_txt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/fmt/trait.Write.html\" title=\"trait core::fmt::Write\">Write</a> for <a class=\"struct\" href=\"zng_txt/struct.Txt.html\" title=\"struct zng_txt::Txt\">Txt</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- doc/trait.impl/rayon/iter/trait.ParallelIterator.js
+++ doc/trait.impl/rayon/iter/trait.ParallelIterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["zng_task",[["impl&lt;T, I&gt; ParallelIterator for <a class=\"struct\" href=\"zng_task/struct.ParallelIteratorWithCtx.html\" title=\"struct zng_task::ParallelIteratorWithCtx\">ParallelIteratorWithCtx</a>&lt;I&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a>,\n    I: ParallelIterator&lt;Item = T&gt;,</div>"]]]]);
+    var implementors = Object.fromEntries([["zng_task",[["impl&lt;T, I&gt; ParallelIterator for <a class=\"struct\" href=\"zng_task/struct.ParallelIteratorWithCtx.html\" title=\"struct zng_task::ParallelIteratorWithCtx\">ParallelIteratorWithCtx</a>&lt;I&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a>,\n    I: ParallelIterator&lt;Item = T&gt;,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
